003b96b716f345d0ed7b283a47718f60c436668b
Caio <caio333119@gmail.com>
2023-08-24 22:08:03 -0300

    Add files via upload

---
+++ Enpont/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Enpont
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton32Bits = new System.Windows.Forms.RadioButton();
+            this.radioButton16Bits = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(183, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Procurar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(173, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(173, 155);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Digite o offset do primeiro texto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Digite o offset do segundo texto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Digite o offset do terceiro texto";
+            // 
+            // radioButton32Bits
+            // 
+            this.radioButton32Bits.AutoSize = true;
+            this.radioButton32Bits.Checked = true;
+            this.radioButton32Bits.Location = new System.Drawing.Point(300, 213);
+            this.radioButton32Bits.Name = "radioButton32Bits";
+            this.radioButton32Bits.Size = new System.Drawing.Size(57, 17);
+            this.radioButton32Bits.TabIndex = 7;
+            this.radioButton32Bits.TabStop = true;
+            this.radioButton32Bits.Text = "32 Bits";
+            this.radioButton32Bits.UseVisualStyleBackColor = true;
+            // 
+            // radioButton16Bits
+            // 
+            this.radioButton16Bits.AutoSize = true;
+            this.radioButton16Bits.Location = new System.Drawing.Point(300, 242);
+            this.radioButton16Bits.Name = "radioButton16Bits";
+            this.radioButton16Bits.Size = new System.Drawing.Size(57, 17);
+            this.radioButton16Bits.TabIndex = 8;
+            this.radioButton16Bits.TabStop = true;
+            this.radioButton16Bits.Text = "16 Bits";
+            this.radioButton16Bits.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 271);
+            this.Controls.Add(this.radioButton16Bits);
+            this.Controls.Add(this.radioButton32Bits);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Encontrar Ponteiros by Angel333119";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton32Bits;
+        private System.Windows.Forms.RadioButton radioButton16Bits;
+    }
+}
+
---
+++ Enpont/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace Enpont
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.KeyPress += TextBox_KeyPress;
+            textBox2.KeyPress += TextBox_KeyPress;
+            textBox3.KeyPress += TextBox_KeyPress;
+        }
+
+        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char inputChar = e.KeyChar;
+            if (!IsHexadecimalChar(inputChar) && !Char.IsControl(inputChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private bool IsHexadecimalChar(char inputChar)
+        {
+            return (inputChar >= '0' && inputChar <= '9') || (inputChar >= 'A' && inputChar <= 'F') || (inputChar >= 'a' && inputChar <= 'f');
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Todos os arquivos|*.*";
+            openFileDialog.Title = "Selecione um arquivo";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    BinaryReader br = new BinaryReader(stream);
+
+                    FileInfo fileInfo = new FileInfo(filePath);
+
+                    long tamanhoarquivo = fileInfo.Length;
+
+                    long offset1 = long.Parse(textBox1.Text, NumberStyles.HexNumber);
+                    long offset2 = long.Parse(textBox2.Text, NumberStyles.HexNumber);
+                    long offset3 = long.Parse(textBox3.Text, NumberStyles.HexNumber);
+
+                    long sub1 = offset2 - offset1;
+                    long sub2 = offset3 - offset2;
+                    long sub3 = offset3 - offset1;
+
+                    long offsetponteiro1 = 0;
+                    long offsetponteiro2 = 0;
+                    long offsetponteiro3 = 0;
+
+                    bool foundOffsets = false;
+
+                    if (radioButton32Bits.Checked)
+                    {
+                        var result = MessageBox.Show("O arquivo é Little Endian?", "Seleção de Endian", MessageBoxButtons.YesNoCancel);
+
+                        bool isLittleEndian = false;
+                        if (result == DialogResult.Yes)
+                        {
+                            isLittleEndian = true;
+                        }
+                        else if (result == DialogResult.No)
+                        {
+                            isLittleEndian = false;
+                        }
+                        else
+                        {
+                            return;
+                        }
+
+                        for (long currentPosition = 0; currentPosition < tamanhoarquivo - 4; currentPosition += 1)
+                        {
+                            br.BaseStream.Position = currentPosition;
+
+                            int valor1;
+
+                            if (isLittleEndian)
+                            {
+                                valor1 = br.ReadInt32();
+                            }
+                            else
+                            {
+                                byte[] bytes1 = br.ReadBytes(4);
+                                Array.Reverse(bytes1);
+                                valor1 = BitConverter.ToInt32(bytes1, 0);
+                            }
+
+                            if (valor1 == offset1)
+                            {
+                                offsetponteiro1 = currentPosition;
+
+                                for (long currentPosition2 = offsetponteiro1 + 4; currentPosition2 < tamanhoarquivo - 4; currentPosition2 += 1)
+                                {
+                                    br.BaseStream.Position = currentPosition2;
+
+                                    int valor2;
+
+                                    if (isLittleEndian)
+                                    {
+                                        valor2 = br.ReadInt32();
+                                    }
+                                    else
+                                    {
+                                        byte[] bytes2 = br.ReadBytes(4);
+                                        Array.Reverse(bytes2);
+                                        valor2 = BitConverter.ToInt32(bytes2, 0);
+                                    }
+
+                                    if (valor2 == offset2)
+                                    {
+                                        offsetponteiro2 = currentPosition2;
+
+                                        for (long currentPosition3 = offsetponteiro2 + 4; currentPosition3 < tamanhoarquivo - 4; currentPosition3 += 1)
+                                        {
+                                            br.BaseStream.Position = currentPosition3;
+
+                                            int valor3;
+
+                                            if (isLittleEndian)
+                                            {
+                                                valor3 = br.ReadInt32();
+                                            }
+                                            else
+                                            {
+                                                byte[] bytes3 = br.ReadBytes(4);
+                                                Array.Reverse(bytes3);
+                                                valor3 = BitConverter.ToInt32(bytes3, 0);
+                                            }
+
+                                            if (valor3 == offset3)
+                                            {
+                                                // Encontrou o terceiro ponteiro
+                                                offsetponteiro3 = currentPosition3;
+
+                                                MessageBox.Show("Possíveis offsets dos ponteiros:\n\nOffset primeiro ponteiro: " + offsetponteiro1.ToString("X2") + "\nOffset segundo ponteiro: " + offsetponteiro2.ToString("X2") + "\nOffset terceiro ponteiro: " + offsetponteiro3.ToString("X2"));
+
+                                                foundOffsets = true;
+                                                break;
+                                            }
+                                        }
+                                        if (foundOffsets)
+                                            break;
+                                    }
+                                }
+                                if (foundOffsets)
+                                    break;
+                            }
+                        }
+                        if (!foundOffsets)
+                        {
+                            MessageBox.Show("Infelizmente não encontrei os offsets dos ponteiros em 32 bits!");
+                        }
+                    }
+
+                    if (radioButton16Bits.Checked)
+                    {
+                        var result = MessageBox.Show("O arquivo é Little Endian?", "Seleção de Endian", MessageBoxButtons.YesNoCancel);
+
+                        bool isLittleEndian = false;
+                        if (result == DialogResult.Yes)
+                        {
+                            isLittleEndian = true;
+                        }
+                        else if (result == DialogResult.No)
+                        {
+                            isLittleEndian = false;
+                        }
+                        else
+                        {
+                            return;
+                        }
+
+                        bool foundOffsets16Bits = false;
+
+                        for (long currentPosition16Bits = 0; currentPosition16Bits < tamanhoarquivo - 2; currentPosition16Bits += 1)
+                        {
+                            br.BaseStream.Position = currentPosition16Bits;
+
+                            short valor16Bits;
+
+                            if (isLittleEndian)
+                            {
+                                valor16Bits = br.ReadInt16();
+                            }
+                            else
+                            {
+                                byte[] bytes16Bits = br.ReadBytes(2);
+                                Array.Reverse(bytes16Bits);
+                                valor16Bits = BitConverter.ToInt16(bytes16Bits, 0);
+                            }
+
+                            if (valor16Bits == offset1)
+                            {
+                                offsetponteiro1 = currentPosition16Bits;
+
+                                for (long currentPosition216Bits = offsetponteiro1 + 2; currentPosition216Bits < tamanhoarquivo - 2; currentPosition216Bits += 1)
+                                {
+                                    br.BaseStream.Position = currentPosition216Bits;
+
+                                    short valor216Bits;
+
+                                    if (isLittleEndian)
+                                    {
+                                        valor216Bits = br.ReadInt16();
+                                    }
+                                    else
+                                    {
+                                        byte[] bytes216Bits = br.ReadBytes(2);
+                                        Array.Reverse(bytes216Bits);
+                                        valor216Bits = BitConverter.ToInt16(bytes216Bits, 0);
+                                    }
+
+                                    if (valor216Bits == offset2)
+                                    {
+                                        offsetponteiro2 = currentPosition216Bits;
+
+                                        for (long currentPosition316Bits = offsetponteiro2 + 2; currentPosition316Bits < tamanhoarquivo - 2; currentPosition316Bits += 1)
+                                        {
+                                            br.BaseStream.Position = currentPosition316Bits;
+
+                                            short valor316Bits;
+
+                                            if (isLittleEndian)
+                                            {
+                                                valor316Bits = br.ReadInt16();
+                                            }
+                                            else
+                                            {
+                                                byte[] bytes316Bits = br.ReadBytes(2);
+                                                Array.Reverse(bytes316Bits);
+                                                valor316Bits = BitConverter.ToInt16(bytes316Bits, 0);
+                                            }
+
+                                            if (valor316Bits == offset3)
+                                            {
+                                                offsetponteiro3 = currentPosition316Bits;
+
+                                                MessageBox.Show("Possíveis offsets dos ponteiros:\n\nOffset primeiro ponteiro: " + offsetponteiro1.ToString("X2") + "\nOffset segundo ponteiro: " + offsetponteiro2.ToString("X2") + "\nOffset terceiro ponteiro: " + offsetponteiro3.ToString("X2"));
+
+                                                foundOffsets16Bits = true;
+                                                break;
+                                            }
+                                        }
+                                        if (foundOffsets16Bits)
+                                            break;
+                                    }
+                                }
+                                if (foundOffsets16Bits)
+                                    break;
+                            }
+                        }
+                        if (!foundOffsets16Bits)
+                        {
+                            MessageBox.Show("Infelizmente não encontrei os offsets dos ponteiros em 16 bits!");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
